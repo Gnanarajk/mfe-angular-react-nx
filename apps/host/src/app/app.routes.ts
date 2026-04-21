8c7e01cb766dc62c3af1f8b461f3d9fb242a4d52
Gnanaraj <gnanaraj.k@gmail.com>
2026-04-21 18:04:54 +0530

    remote 2 and remote 3 working

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -4,13 +4,6 @@ import { loadRemote } from '@module-federation/enhanced/runtime';
 import { ReactWrapper } from './react-wrapper/react-wrapper';
 
 export const appRoutes: Route[] = [
-  {
-    path: 'remote2',
-    loadChildren: () =>
-      loadRemote<typeof import('remote2/Routes')>('remote2/Routes').then(
-        (m) => m!.remoteRoutes
-      ),
-  },
   // {
   //   path: 'remote1',
   //   loadChildren: () =>
@@ -18,14 +11,21 @@ export const appRoutes: Route[] = [
   //       (m) => m!.remoteRoutes
   //     ),
   // },
-  // {
-  //   path: 'remote3',
-  //   component: ReactWrapper, // the Angular wrapper component which will load any React web component
-  //   data: {
-  //     elementName: 'wc-remote3', // the name of the custom element which is a React web component
-  //     loadChildren: () => import('remote3/Module'), // the dynamic import of the React web component
-  //   },
-  // },
+  {
+    path: 'remote2',
+    loadChildren: () =>
+      loadRemote<typeof import('remote2/Routes')>('remote2/Routes').then(
+        (m) => m!.remoteRoutes
+      ),
+  },
+  {
+    path: 'remote3',
+    component: ReactWrapper, // the Angular wrapper component which will load any React web component
+    data: {
+      elementName: 'wc-remote3', // the name of the custom element which is a React web component
+      loadChildren: () => import('remote3/Module'), // the dynamic import of the React web component
+    },
+  },
   {
     path: '',
     component: NxWelcome,
