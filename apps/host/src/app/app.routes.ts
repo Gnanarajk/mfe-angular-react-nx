1be1841f7df4130a0303702673799dcf4c02ace3
Gnanaraj <gnanaraj.k@gmail.com>
2026-04-21 16:51:27 +0530

    Host and Remote2 are working now

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -11,21 +11,21 @@ export const appRoutes: Route[] = [
         (m) => m!.remoteRoutes
       ),
   },
-  {
-    path: 'remote1',
-    loadChildren: () =>
-      loadRemote<typeof import('remote1/Routes')>('remote1/Routes').then(
-        (m) => m!.remoteRoutes
-      ),
-  },
-  {
-    path: 'remote3',
-    component: ReactWrapper, // the Angular wrapper component which will load any React web component
-    data: {
-      elementName: 'wc-remote3', // the name of the custom element which is a React web component
-      loadChildren: () => import('remote3/Module'), // the dynamic import of the React web component
-    },
-  },
+  // {
+  //   path: 'remote1',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('remote1/Routes')>('remote1/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
+  // {
+  //   path: 'remote3',
+  //   component: ReactWrapper, // the Angular wrapper component which will load any React web component
+  //   data: {
+  //     elementName: 'wc-remote3', // the name of the custom element which is a React web component
+  //     loadChildren: () => import('remote3/Module'), // the dynamic import of the React web component
+  //   },
+  // },
   {
     path: '',
     component: NxWelcome,
